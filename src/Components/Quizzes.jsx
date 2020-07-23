90632ae7d59792170ab322f565b92f3b6f5787a5
SamaAlshareef <sama.elsherif@student.guc.edu.eg>
2020-07-23 13:42:10 +0200

    Fixing  Nav 3

--- src/Components/Quizzes.jsx
+++ src/Components/Quizzes.jsx
@@ -10,97 +10,102 @@ class Quizzes extends Component {
 
             <div className="container-fluid pt-5" >
                 <div className="row no-gutters ">
-                <h1 className="header">Recent Quizzes</h1>
-                <div className="card-deck pl-5  pr-5 d-flex justify-content-center ">
+                <h1 className="header">Recent Homeworks</h1>
+                <div className="card-deck pl-5  pr-5d-flex justify-content-center ">
                     
-                    <div className="col-sm-3">
-                    <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
-                        <img src="/images/study2.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
-                        <div className="card-body">
-                        <div className="h-25">
-                            <h5 className="card-title title-font text-center ">Title</h5>
-                            <p className="text-center">Subtitle</p>
-                        </div>
-                           <div className="h-50 ">
-                                <p className="card-text  text-center">Suspendisse in justo mauris.Morbi
-                                        vitae lectus est. Pellentesque
-                                        commodo nisi id erat pretium,
-                                        sit amet facilisis orci condimentum.</p>
-                                <p className="text-center warning">90/100</p>
-                            </div>
-                            <div className="d-flex flex-row justify-content-center">
-                          
-                            <a href="#" class="card-link justify-content-center gold-font">Start Quiz</a>
-                              <img src="/images/enter.png" className="enter-icon"/>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-3">
-                    <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
-                        <img src="/images/study3.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
-                        <div className="card-body">
-                        <div className="h-25">
-                            <h5 className="card-title title-font text-center ">Title</h5>
-                            <p className="text-center">Subtitle</p>
-                        </div>
-                           <div className="h-50">
-                            <p className="card-text text-center">Suspendisse in justo mauris.Morbi
-                                    vitae lectus est. Pellentesque
-                                    commodo nisi id erat pretium,
-                                    sit amet facilisis orci condimentum.</p>
-                           
-                            </div>
-                            <div className="d-flex flex-row justify-content-center">
-                          
-                            <a href="#" class="card-link  justify-content-center gold-font">Start Quiz</a>
-                              <img src="/images/enter.png" className="enter-icon"/>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-3">
-                    <div className="card border border-gold h-100  shadow p-3 mb-5 bg-white rounded">
-                        <img src="/images/study4.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
-                        <div className="card-body">
-                        <div className="h-25">
-                            <h5 className="card-title title-font text-center ">Title</h5>
-                            <p className="text-center">Subtitle</p>
-                        </div>
-                           <div className="h-50">
-                            <p className="card-text  text-center">Suspendisse in justo mauris.Morbi
-                                    vitae lectus est. Pellentesque
-                                    commodo nisi id erat pretium,
-                                    sit amet facilisis orci condimentum.</p>
-                         
-                            </div>
-                            <div className="d-flex flex-row justify-content-center">
-                          
-                            <a href="#" class="card-link  justify-content-center gold-font">Start Quiz</a>
-                              <img src="/images/enter.png" className="enter-icon"/>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
                     <div className="col-sm-3">
                     <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
                         <img src="/images/study1.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
                         <div className="card-body">
                         <div className="h-25">
-                            <h5 className="card-title title-font text-center ">Title</h5>
+                            <h5 className="card-title title-font text-center h-25">Title</h5>
                             <p className="text-center">Subtitle</p>
-                        </div>
-                           <div className="h-50">
+                            </div>
+                            <div className="h-50">
                             <p className="card-text  text-center">Suspendisse in justo mauris.Morbi
                                     vitae lectus est. Pellentesque
                                     commodo nisi id erat pretium,
                                     sit amet facilisis orci condimentum.</p>
-                            <p className="text-center warning">82/100</p>
+                                    <p className="text-center warning">90/100</p>
                             </div>
                             <div className="d-flex flex-row justify-content-center">
-                          
+                            
+                            <a href="#" class="card-link  justify-content-center gold-font">Start Quiz</a>
+                            <img src="/images/enter.png" className="enter-icon"/>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+           
+                    <div className="col-sm-3">
+                    <div className="card border border-gold h-100  shadow p-3 mb-5 bg-white rounded">
+                        <img src="/images/study2.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
+                        <div className="card-body">
+                        <div className="h-25">
+                            <h5 className="card-title title-font text-center h-25">Title</h5>
+                            <p className="text-center">Subtitle</p>
+                            </div>
+                           <div className="h-50">
+                            <p className="card-text text-center">Suspendisse in justo mauris.Morbi
+                                    vitae lectus est. Pellentesque
+                                    commodo nisi id erat pretium,
+                                    sit amet facilisis orci condimentum.</p>
+                                    
+                             </div>
+                             <div className="d-flex flex-row justify-content-center">
+                            
                             <a href="#" class="card-link justify-content-center gold-font">Start Quiz</a>
-                              <img src="/images/enter.png" className="enter-icon"/>
+                            <img src="/images/enter.png" className="enter-icon"/>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+           
+                    <div className="col-sm-3">
+                    <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
+                        <img src="/images/study3.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
+                        <div className="card-body">
+                        <div className="h-25">
+                            <h5 className="card-title title-font text-center h-25">Title</h5>
+                            <p className="text-center">Subtitle</p>
+                            </div>
+                           <div className="h-50">
+                            <p className="card-text text-center">Suspendisse in justo mauris.Morbi
+                                    vitae lectus est. Pellentesque
+                                    commodo nisi id erat pretium,
+                                    sit amet facilisis orci condimentum.</p>
+                                    
+                             </div>
+                             <div className="d-flex flex-row justify-content-center">
+                            
+                            <a href="#" class="card-link justify-content-center gold-font">Start Quiz</a>
+                            <img src="/images/enter.png" className="enter-icon"/>
+                            
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+           
+                    <div className="col-sm-3">
+                    <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
+                        <img src="/images/study4.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
+                        <div className="card-body">
+                        <div className="h-25">
+                            <h5 className="card-title title-font text-center h-25">Title</h5>
+                            <p className="text-center">Subtitle</p>
+                            </div>
+                           <div className="h-50">
+                           <p className="card-text  text-center">Suspendisse in justo mauris.Morbi
+                                    vitae lectus est. Pellentesque
+                                    commodo nisi id erat pretium,
+                                    sit amet facilisis orci condimentum.</p>
+                                    <p className="text-center warning">82/100</p>
+                           </div>
+                          
+                           <div className="d-flex flex-row justify-content-center">
+                           
+                            <a href="#" class="card-link  justify-content-center gold-font">Start Quiz</a>
+                            <img src="/images/enter.png" className="enter-icon"/>
                             </div>
                         </div>
                         </div>
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 
-class Videos extends Component {
+class HomeWorks extends Component {
     constructor(props){
         super(props);
        
@@ -11,42 +11,54 @@ class Videos extends Component {
             <div className="container-fluid pt-5" >
                 <div className="row no-gutters ">
                 <h1 className="header">Recent Videos</h1>
-                <div className="card-deck pl-5  pr-5 d-flex justify-content-center ">
+                <div className="card-deck pl-5  pr-5d-flex justify-content-center ">
                     
                     <div className="col-sm-3">
-                    <div className="card border border-gold shadow h-100 p-3 mb-5 bg-white rounded">
+                    <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
                         <img src="/images/study1.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
                         <div className="card-body">
                         <div className="h-25">
                             <h5 className="card-title title-font text-center h-25">Title</h5>
                             <p className="text-center">Subtitle</p>
                             </div>
-                            <div className="h-25">
-                            <p className="card-text  text-center ">Suspendisse in justo mauris.Morbi
+                            <div className="h-50">
+                            <p className="card-text  text-center">Suspendisse in justo mauris.Morbi
                                     vitae lectus est. Pellentesque
                                     commodo nisi id erat pretium,
                                     sit amet facilisis orci condimentum.</p>
+                                  
+                            </div>
+                            <div className="d-flex flex-row justify-content-center">
+                            
+                           
                             </div>
                         </div>
                         </div>
                     </div>
+           
                     <div className="col-sm-3">
-                    <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
+                    <div className="card border border-gold h-100  shadow p-3 mb-5 bg-white rounded">
                         <img src="/images/study2.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
                         <div className="card-body">
                         <div className="h-25">
                             <h5 className="card-title title-font text-center h-25">Title</h5>
                             <p className="text-center">Subtitle</p>
                             </div>
-                            <div className="h-25">
-                            <p className="card-text  text-center ">Suspendisse in justo mauris.Morbi
+                           <div className="h-50">
+                            <p className="card-text text-center">Suspendisse in justo mauris.Morbi
                                     vitae lectus est. Pellentesque
                                     commodo nisi id erat pretium,
                                     sit amet facilisis orci condimentum.</p>
+                                   
+                             </div>
+                             <div className="d-flex flex-row justify-content-center">
+                            
+                           
                             </div>
                         </div>
                         </div>
                     </div>
+           
                     <div className="col-sm-3">
                     <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
                         <img src="/images/study3.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
@@ -55,36 +67,46 @@ class Videos extends Component {
                             <h5 className="card-title title-font text-center h-25">Title</h5>
                             <p className="text-center">Subtitle</p>
                             </div>
-                            <div className="h-25">
-                            <p className="card-text  text-center ">Suspendisse in justo mauris.Morbi
+                           <div className="h-50">
+                            <p className="card-text text-center">Suspendisse in justo mauris.Morbi
                                     vitae lectus est. Pellentesque
                                     commodo nisi id erat pretium,
                                     sit amet facilisis orci condimentum.</p>
+                                   
+                             </div>
+                             <div className="d-flex flex-row justify-content-center">
+                            
+                         
+                            
                             </div>
                         </div>
                         </div>
                     </div>
+           
                     <div className="col-sm-3">
-                    <div className="card border border-gold shadow h-100 p-3 mb-5 bg-white rounded">
+                    <div className="card border border-gold h-100 shadow p-3 mb-5 bg-white rounded">
                         <img src="/images/study4.jpg" className="border border-non-gold card-img d-flex justify-content-center " alt="..."/>
                         <div className="card-body">
                         <div className="h-25">
                             <h5 className="card-title title-font text-center h-25">Title</h5>
                             <p className="text-center">Subtitle</p>
                             </div>
-                            <div className="h-25">
-                            <p className="card-text  text-center ">Suspendisse in justo mauris.Morbi
+                           <div className="h-50">
+                           <p className="card-text  text-center">Suspendisse in justo mauris.Morbi
                                     vitae lectus est. Pellentesque
                                     commodo nisi id erat pretium,
                                     sit amet facilisis orci condimentum.</p>
+                                    
+                           </div>
+                          
+                           <div className="d-flex flex-row justify-content-center">
+                         
                             </div>
                         </div>
                         </div>
                     </div>
-           
-                    
-                </div>
-                <img src="/images/pagination.png" className="d-flex justify-content-center pagination-icon"/>
+                    <img src="/images/pagination.png" className="d-flex justify-content-center pagination-icon"/>
+          </div>
                 </div>
             
           </div>
@@ -94,5 +116,5 @@ class Videos extends Component {
     }
 }
  
-export default Videos;
+export default HomeWorks;
 
